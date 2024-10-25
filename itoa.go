98e84a6d18e361e55b9aefc1fa5e82de1009f688
Eliza John <112093038+ejmilli@users.noreply.github.com>
2024-10-25 20:16:28 +0300

    itoa understanding is well established

---
+++ itoa.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println(Itoa(12345))
+	fmt.Println(Itoa(0))
+	fmt.Println(Itoa(-1234))
+	fmt.Println(Itoa(987654321))
+}
+
+func Itoa(n int) string {
+	if n == 0 {
+		return "0"
+	}
+
+	isNegative := n < 0
+	if isNegative {
+		n = -n
+	}
+
+	result := ""
+	for n > 0 {
+		digit := n % 10
+		result = string('0'+digit) + result
+		n /= 10
+	}
+
+	if isNegative {
+		result = "-" + result
+	}
+
+	return result
+
+}
