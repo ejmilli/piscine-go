bb56eca7bf089180a859f6411d5ea89ef37aee09
Eliza John <112093038+ejmilli@users.noreply.github.com>
2024-10-26 22:25:14 +0300

    similar to wdmatch

---
+++ hiddenp/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+ if len(os.Args) != 3 {
+   return
+}
+
+str1 := os.Args[1]
+str2 := os.Args[2]
+
+i := 0
+j := 0
+
+
+for i < len(str1) && j < len(str2) {
+     if str1[i] == str2[j] {
+    i++
+}
+j++
+}
+
+if i == len(str1) {
+  z01.PrintRune('1')
+} else {
+z01.PrintRune('0')
+}
+
+}
