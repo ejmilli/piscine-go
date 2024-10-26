e8b1625dd6795602af4de8163ab8d76b81eaa487
Eliza John <112093038+ejmilli@users.noreply.github.com>
2024-10-26 11:11:34 +0300

    understand it a bit

---
+++ zipstring/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	fmt.Println(ZipString("YouuungFellllas"))
+	fmt.Println(ZipString("Thee quuick browwn fox juumps over the laaazy dog"))
+	fmt.Println(ZipString("Helloo Therre!"))
+}
+
+func ZipString(s string) string {
+	result := ""
+	i := 0
+
+	for i < len(s) {
+		char := s[i]
+		count := 1
+		for i+1 < len(s) && s[i+1] == char {
+			count++
+			i++
+		}
+
+		result += strconv.Itoa(count) + string(char)
+		i++
+	}
+	return result
+}
