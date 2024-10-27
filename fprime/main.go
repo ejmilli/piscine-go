7f006ba2d691d75228f6a8935e5ca2d298f05c09
Eliza John <112093038+ejmilli@users.noreply.github.com>
2024-10-27 11:03:16 +0200

    fprime completed

---
+++ fprime/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func printFactor(n int) {
+
+	if n <= 1 {
+		return
+	}
+
+	factor := 2
+	for n > 1 {
+		if n%factor == 0 {
+			fmt.Print(factor)
+			n /= factor
+			if n > 1 {
+				fmt.Print("*")
+			}
+
+		} else {
+			factor++
+		}
+	}
+}
+
+func main() {
+	if len(os.Args) == 2 {
+		input, err := strconv.Atoi(os.Args[1])
+		if err == nil {
+			printFactor(input)
+		}
+		fmt.Print()
+	}
+}
