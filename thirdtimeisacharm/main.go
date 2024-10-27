ac2f6f7f8be3da84a4c71e6db949c374a93d052c
Eliza John <112093038+ejmilli@users.noreply.github.com>
2024-10-27 18:24:35 +0200

    thirdtimeisacharm is done

---
+++ thirdtimeisacharm/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Print(ThirdTimeIsACharm("123456789"))
+	fmt.Print(ThirdTimeIsACharm(""))
+	fmt.Print(ThirdTimeIsACharm("a b c d e f"))
+	fmt.Print(ThirdTimeIsACharm("12"))
+}
+
+func ThirdTimeIsACharm(str string) string {
+	if len(str) == 0 {
+
+		return "" + "\n"
+	}
+
+	if len(str) < 3 {
+		return "" + "\n"
+	}
+
+	var result string
+	for i := 2; i < len(str); i += 3 {
+		result += string(str[i])
+
+	}
+
+	return result + "\n"
+}
