d310d3e7120aebb5a6f37979d28e04197966664c
Eliza John <112093038+ejmilli@users.noreply.github.com>
2024-10-31 10:28:27 +0200

    new folder

---
+++ milli/milli.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+
+
+func main() {
+
+	if len(os.Args) != 2 {
+		return
+	}
+
+	str := os.Args[1]
+	inWord := false
+
+	for i, char := range str {
+		if char == ' ' || char == '\t' {
+			if inWord && i != 0 && str[i-1] != ' ' && str[i-1] != '\t' {
+				z01.PrintRune(' ')
+				z01.PrintRune(' ')
+				z01.PrintRune(' ')
+			}
+		} else {
+			z01.PrintRune(char)
+			inWord = true
+		}
+	}
+	z01.PrintRune('\n')
+
+}
