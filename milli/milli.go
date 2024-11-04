f4be4bb903a99fb77ec4aad4a76c2ccd8a8b495f
Eliza John <112093038+ejmilli@users.noreply.github.com>
2024-11-04 10:07:24 +0200

    make changes

--- milli/milli.go
+++ milli/milli.go
@@ -1,3 +1,4 @@
+
 package main
 
 import (
@@ -5,63 +6,34 @@ import (
 
 	"github.com/01-edu/z01"
 )
-
-
-func isPrime(n int) bool {
-
-	for i := 2; i*i <= n; i++ {
-		if n%i == 0 {
-			return false
-		}
-	}
-	return true
-}
-
-func isSum(n int) int {
-	sum := 0
-	for i := 2; i <= n; i++ {
-		if isPrime(i) {
-			sum += i
-		}
-	}
-	return sum
-}
-
-func isDigits(n int) {
-
-	if n == 0 {
-		return
-	}
-	isDigits(n / 10)
-	z01.PrintRune(rune(n%10 + '0'))
-}
-
-func printNumber(n int) {
-	if n == 0 {
-		z01.PrintRune('0')
-		return
-	}
-
-	isDigits(n)
-}
+ 
 
 func main() {
-	if len(os.Args) != 2 {
-		return
-	}
 
-	n := 0
-	str := os.Args[1]
+ if len(os.Args) != 4 {
+  return 
+}
 
-	for _, char := range str {
-		if char < '0' || char > '9' {
-			z01.PrintRune('0')
-			return
-		}
+str1 := os.Args[1]
+str2 := os.Args[2]
+str3 := os.Args[3]
 
-		n = n*10 + int(char-'0')
-	}
-	result := isSum(n)
-	printNumber(result)
-	z01.PrintRune('\n')
+if len(str2) != 1 && len(str3) != 1 {
+ return
+}
+
+var result string 
+
+for _, char := range str1 {
+   if string(char) == str2 {
+   result += str3
+} else {
+  result += string(char)
+}
+}
+
+for _, char := range result {
+  z01.PrintRune(char)
+}
+
 }
