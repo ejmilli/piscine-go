02049286241dbb76dbc13c045cfe8f6029ceac5a
Eliza John <112093038+ejmilli@users.noreply.github.com>
2024-11-10 17:02:56 +0200

    .....

--- milli/milli.go
+++ milli/milli.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	
+	"strconv"
 )
 
 func main() {
@@ -14,12 +14,48 @@ func main() {
 
 func FromTo(from int, to int) string {
    if from < 0 || from > 99 || to < 0 || to > 99 {
-    return "Invalid" + "\n"
+    return "Invalid\n" 
    }
 
-   if from == to {
-       
+   var result string
 
-   }
+  if from == to {
+    if from < 10 {
+      result += "0" + strconv.Itoa(from)
+    } else {
+      result +=  strconv.Itoa(from)
+    }
+  }
 
+  if from < to {
+    for i := from; i <= to ; i++ {
+      if i < 10 {
+        result += "0" + strconv.Itoa(i)
+      } else {
+        result +=  strconv.Itoa(i)
+      }
+
+      if i != to {
+        result += ", "
+      }
+    }
+  }
+
+  if from > to {
+    for i := from; i >= to; i-- {
+
+      if i < 10 {
+        result += "0" + strconv.Itoa(i)
+    } else {
+      result +=  strconv.Itoa(i)
+    }
+   
+    if i != to {
+      result += ", "
+    }
+  }
+
+
+}
+return result + "\n"
 }
