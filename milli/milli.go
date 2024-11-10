d223dd107442513703962b1cfeb8de0801716a94
Eliza John <112093038+ejmilli@users.noreply.github.com>
2024-11-10 23:10:55 +0200

    ....

--- milli/milli.go
+++ milli/milli.go
@@ -5,23 +5,43 @@ import (
 )
 
 func main() {
-	fmt.Println(ConcatSlice([]int{1, 2, 3}, []int{4, 5, 6}))
-	fmt.Println(ConcatSlice([]int{}, []int{4, 5, 6, 7, 8, 9}))
-	fmt.Println(ConcatSlice([]int{1, 2, 3}, []int{}))
+	fmt.Println(WeAreUnique("foo", "boo"))
+	fmt.Println(WeAreUnique("", ""))
+	fmt.Println(WeAreUnique("abc", "def"))
 }
 
-func ConcatSlice(slice1, slice2 []int) []int {
-   
+func isContain(s string, r rune) bool {
+	for _, char := range s {
+		if char == r {
+			return true
+		}
+	}
+	return false
+}
 
-  var result []int
 
-  for _, char := range slice1 {
-    result = append(result, char)
+
+func WeAreUnique(str1, str2 string) int {
+
+if len(str1) == 0 && len(str2) == 0 {
+  return -1
+}
+
+if str1 == str2 {
+  return 0
+}
+
+var result string
+
+for _, char := range str1 {
+  if !isContain(str2, char) && !isContain(result, char) {
+    result += string(char)
   }
-
-  for _, char := range slice2 {
-    result = append(result, char)
+}
+for _, char := range str2 {
+  if !isContain(str1, char) && !isContain(result, char) {
+    result += string(char)
   }
-
-  return result
+}
+return len(result)
 }
