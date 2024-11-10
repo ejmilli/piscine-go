89b0dffa5ab33ce520665615104af10658a33de9
Eliza John <112093038+ejmilli@users.noreply.github.com>
2024-11-10 21:54:05 +0200

    ......

--- milli/milli.go
+++ milli/milli.go
@@ -2,29 +2,29 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 )
 
 func main() {
-	fmt.Print(ThirdTimeIsACharm("123456789"))
-	fmt.Print(ThirdTimeIsACharm(""))
-	fmt.Print(ThirdTimeIsACharm("a b c d e f"))
-	fmt.Print(ThirdTimeIsACharm("12"))
+	fmt.Println(ZipString("YouuungFellllas"))
+	fmt.Println(ZipString("Thee quuick browwn fox juumps over the laaazy dog"))
+	fmt.Println(ZipString("Helloo Therre!"))
 }
 
-func ThirdTimeIsACharm(str string) string {
+func ZipString(s string) string {
 
-if len(str) == 0 {
-  return "\n"
+var result string 
+i := 0 
+
+for i < len(s) {
+  char := s[i]
+  count := 1
+  for i+1 < len(s) && s[i+1] == char {
+    count++
+    i++
+  }
+  result += strconv.Itoa(count) + string(char)
+  i++
 }
-
-if len(str) < 3 {
-  return "\n"
-}
-
-var result string
-
-for i := 2; i < len(str); i+=3 {
-  result += string(str[i])
-}
-return result + "\n"
+return result
 }
