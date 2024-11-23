719c991f67b54560c497e81e23442eb5f6cffedf
Eliza John <112093038+ejmilli@users.noreply.github.com>
2024-11-23 20:29:57 +0200

    changes

--- milli/milli.go
+++ milli/milli.go
@@ -1,34 +1,37 @@
 package main
 
 import (
-	"fmt"
+	"os"
+
+	"github.com/01-edu/z01"
 )
 
-func main() {
-	fmt.Println(ConcatAlternate([]int{1, 2, 3}, []int{4, 5, 6}))
-	fmt.Println(ConcatAlternate([]int{2, 4, 6, 8, 10}, []int{1, 3, 5, 7, 9, 11}))
-	fmt.Println(ConcatAlternate([]int{1, 2, 3}, []int{4, 5, 6, 7, 8, 9}))
-	fmt.Println(ConcatAlternate([]int{1, 2, 3}, []int{}))
+func isContain(s string, r rune) bool {
+   for _, char := range s {
+		if char == r {
+			return true
+		}
+	 }
+	 return false
 }
 
-func ConcatAlternate(s1, s2 []int) []int {
+func main() {
 
-	var result []int
+	if len(os.Args) != 3 {
+		return
+	}
+	
+ str1 := os.Args[1]
+ str2 := os.Args[2]
 
-	if len(s1) >= len(s2) {
-		for i := 0; i < len(s1); i++ {
-			result = append(result, s1[i])
-			if i < len(s2) {
-				result = append(result, s2[i])
-			}
-		}
-	} else if len(s2) > len(s1) {
-		for i := 0; i < len(s2); i++ {
-			result = append(result, s2[i])
-			if i < len(s1) {
-				result = append(result, s1[i])
-			}
-		}
-	} 
-return result
+ var result string
+ for _, char := range str1 {
+  if isContain(str2, char) && !isContain(result, char) {
+		result += string(char)
+	}
+ }
+
+ for _, char := range result {
+	z01.PrintRune(char)
+}
 }
