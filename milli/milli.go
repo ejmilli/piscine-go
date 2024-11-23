2e18676fc811a67760844b128c8a3ba7cdf7c6ea
Eliza John <112093038+ejmilli@users.noreply.github.com>
2024-11-23 20:08:40 +0200

    changes

--- milli/milli.go
+++ milli/milli.go
@@ -5,39 +5,30 @@ import (
 )
 
 func main() {
-	input1 := []uint{2, 3, 1, 1, 4}
-	fmt.Println(CanJump(input1)) // Should print: true
-
-	input2 := []uint{3, 2, 1, 0, 4}
-	fmt.Println(CanJump(input2)) // Should print: false
-
-	input3 := []uint{0}
-	fmt.Println(CanJump(input3))  // Should print: true
+	fmt.Println(ConcatAlternate([]int{1, 2, 3}, []int{4, 5, 6}))
+	fmt.Println(ConcatAlternate([]int{2, 4, 6, 8, 10}, []int{1, 3, 5, 7, 9, 11}))
+	fmt.Println(ConcatAlternate([]int{1, 2, 3}, []int{4, 5, 6, 7, 8, 9}))
+	fmt.Println(ConcatAlternate([]int{1, 2, 3}, []int{}))
 }
 
-func max(a, b int) int {
-	if a > b {
-		return a
-	}
-	return b
-}
+func ConcatAlternate(s1, s2 []int) []int {
 
-func CanJump(steps []uint) bool {
+	var result []int
 
-if len(steps) == 0 {
-	return false
-}
-
-maxjump := 0
-
-for i := 0; i < len(steps); i++ {
-	if i > maxjump {
-		return false
-	}
-maxjump = max(maxjump, i+int(steps[i]))
-if maxjump >= len(steps)-1 {
-	return true
-}
-}  
-return false
+	if len(s1) >= len(s2) {
+		for i := 0; i < len(s1); i++ {
+			result = append(result, s1[i])
+			if i < len(s2) {
+				result = append(result, s2[i])
+			}
+		}
+	} else if len(s2) > len(s1) {
+		for i := 0; i < len(s2); i++ {
+			result = append(result, s2[i])
+			if i < len(s1) {
+				result = append(result, s1[i])
+			}
+		}
+	} 
+return result
 }
