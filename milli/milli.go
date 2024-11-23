814c05baa0d9d44c49595c569b0125dba5ced212
Eliza John <112093038+ejmilli@users.noreply.github.com>
2024-11-23 19:28:26 +0200

    char

--- milli/milli.go
+++ milli/milli.go
@@ -15,35 +15,32 @@ func isUpper(s rune) bool {
 	return s >= 'A' && s <= 'Z'
 }
 
-func reverseStrCap(s string ) string {
-result := []rune(s)
-for i := 0; i < len(result); i++ {
-	if i == len(result)-1 || result[i+1] == ' '{
-		if isLower(result[i]) {
-			result[i] -= 32
-		} 
-		}else if isUpper(result[i]) {
-			result[i] += 32
-		
+func reverseStrCap(s string) string {
+  result := []rune(s)
+
+	for i := 0 ; i < len(result); i++ {
+		if i == len(result)-1 || result[i+1] == ' ' {
+			if isLower(result[i]) {
+				result[i] -= 32
+			}
+		} else if isUpper(result[i]) {
+       result[i] += 32
+		}
 	}
-}
- return string(result) 
+	return string(result)
 }
 
 func main() {
 
 	if len(os.Args) > 1 {
-     for _, char := range os.Args[1:] {
-			 result := reverseStrCap(char)
+		for _ , char := range os.Args[1:] {
+			result := reverseStrCap(char)
 
-
-			 for _, char := range result {
+			for _, char := range result {
 				z01.PrintRune(char)
-			 }
-			 z01.PrintRune('\n')
-
-		 }
-
+			} 
+		}
 		
 	}
+
 }
