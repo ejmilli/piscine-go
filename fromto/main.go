6d4fe408e2ce9aa5c1e16db0b36fd077d331a2f4
Eliza John <112093038+ejmilli@users.noreply.github.com>
2024-11-24 17:51:11 +0200

    ..,

---
+++ fromto/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	fmt.Print(FromTo(1, 10))
+	fmt.Print(FromTo(10, 1))
+	fmt.Print(FromTo(10, 10))
+	fmt.Print(FromTo(100, 10))
+}
+
+func FromTo(from int, to int) string {
+
+if from < 0 || from > 99 || to < 0 || to > 99 {
+	return "Invalid" + "\n"
+}
+
+var result string
+
+if from == to {
+	if from < 10 {
+      result += "0" + strconv.Itoa(from)
+	} else {
+		result += strconv.Itoa(from)
+	}
+}
+
+if from < to {
+	for i := from; i <= to ; i++ {
+		if i < 10 {
+			result += "0" + strconv.Itoa(i)
+		} else {
+			result +=  strconv.Itoa(i)
+		}
+
+		if i != to {
+			result += ", "
+		}
+	}
+}
+
+if from  > to {
+	for i := from; i >= to ; i-- {
+		if i < 10 {
+			result += "0" + strconv.Itoa(i)
+		} else {
+			result +=  strconv.Itoa(i)
+		}
+
+		if i != to {
+			result += ", "
+		}
+	}
+}
+
+return result + "\n"
+}
