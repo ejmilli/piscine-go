ab2133bfa221a8eb2aadf14cfdbda1f03f28d008
Eliza John <112093038+ejmilli@users.noreply.github.com>
2024-11-24 17:36:50 +0200

    changes

--- milli/milli.go
+++ milli/milli.go
@@ -1,37 +1,40 @@
 package main
 
 import (
-	"os"
-
-	"github.com/01-edu/z01"
+	"fmt"
 )
 
-func isContain(s string, r rune) bool {
-   for _, char := range s {
-		if char == r {
-			return true
-		}
-	 }
-	 return false
-}
-
 func main() {
-
-	if len(os.Args) != 3 {
-		return
-	}
-	
- str1 := os.Args[1]
- str2 := os.Args[2]
-
- var result string
- for _, char := range str1 {
-  if isContain(str2, char) && !isContain(result, char) {
-		result += string(char)
-	}
- }
-
- for _, char := range result {
-	z01.PrintRune(char)
+	fmt.Println(Itoa(12345))
+	fmt.Println(Itoa(0))
+	fmt.Println(Itoa(-1234))
+	fmt.Println(Itoa(987654321))
 }
+
+func Itoa(n int) string {
+
+	if n == 0 {
+		return "0"
+	}
+   
+isNegative := n < 0
+
+if isNegative {
+	n = - n
+}
+
+var result string
+
+for n > 0 {
+	digits := n %10
+  result = string(rune(digits + '0')) + result
+	n/=10
+}
+
+if isNegative {
+	result = "-" + result
+}
+
+
+return result
 }
