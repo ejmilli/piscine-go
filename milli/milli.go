9e9f74a7a58b7f473e051625766526cb25a9764e
Eliza John <112093038+ejmilli@users.noreply.github.com>
2024-12-07 20:37:55 +0200

    .,.,

--- milli/milli.go
+++ milli/milli.go
@@ -1,31 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	Chunk([]int{}, 10)
-	Chunk([]int{0, 1, 2, 3, 4, 5, 6, 7}, 0)
-	Chunk([]int{0, 1, 2, 3, 4, 5, 6, 7}, 3)
-	Chunk([]int{0, 1, 2, 3, 4, 5, 6, 7}, 5)
-	Chunk([]int{0, 1, 2, 3, 4, 5, 6, 7}, 4)
+	fmt.Println(ConcatAlternate([]int{1, 2, 3}, []int{4, 5, 6}))
+	fmt.Println(ConcatAlternate([]int{2, 4, 6, 8, 10}, []int{1, 3, 5, 7, 9, 11}))
+	fmt.Println(ConcatAlternate([]int{1, 2, 3}, []int{4, 5, 6, 7, 8, 9}))
+	fmt.Println(ConcatAlternate([]int{1, 2, 3}, []int{}))
 }
 
 
-func Chunk(slice []int, size int) {
- 
-	if size == 0 {
-		fmt.Println()
-		return
-	}
+func ConcatAlternate(s1, s2 []int) []int {
 
-	var chunks [][]int
+	var result []int
 
-	for i := 0; i < len(slice); i+= size {
-		end := i + size
-		if end > len(slice) {
-			end = len(slice)
+
+	if len(s1) >= len(s2) {
+		for i := 0; i < len(s1); i++ {
+			result = append(result, s1[i])
+			if i < len(s2) {
+				result = append(result, s2[i])
+			}
 		}
-		chunks = append(chunks, slice[i:end])
-	}
-	   fmt.Println(chunks)
+	} else if len(s2) > len(s1) {
+		for i := 0; i < len(s2); i++ {
+			result = append(result, s2[i])
+			if i < len(s1) {
+				result = append(result, s1[i])
+			}
+		}
+	} 
+return result
 }
