329cbfbb676c5f7653252ae227e6f6e7f0e3ba09
Eliza John <112093038+ejmilli@users.noreply.github.com>
2024-12-07 20:33:05 +0200

    ..

--- milli/milli.go
+++ milli/milli.go
@@ -1,45 +1,31 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	input1 := []uint{2, 3, 1, 1, 4}
-	fmt.Println(CanJump(input1)) // Should print: true
-
-	input2 := []uint{3, 2, 1, 0, 4}
-	fmt.Println(CanJump(input2)) // Should print: false
-
-	input3 := []uint{0}
-	fmt.Println(CanJump(input3))  // Should print: true
+	Chunk([]int{}, 10)
+	Chunk([]int{0, 1, 2, 3, 4, 5, 6, 7}, 0)
+	Chunk([]int{0, 1, 2, 3, 4, 5, 6, 7}, 3)
+	Chunk([]int{0, 1, 2, 3, 4, 5, 6, 7}, 5)
+	Chunk([]int{0, 1, 2, 3, 4, 5, 6, 7}, 4)
 }
 
 
-func max(a, b int) int {
-
-	if a > b {
-		return a
-	}
-	return b
-}
-
-func CanJump(steps []uint) bool {
-
-	if len(steps) == 0 {
-		return false
+func Chunk(slice []int, size int) {
+ 
+	if size == 0 {
+		fmt.Println()
+		return
 	}
 
-	maxjump := 0
+	var chunks [][]int
 
-	for i := 0; i < len(steps); i++ {
-	if	i > maxjump {
-          return false
+	for i := 0; i < len(slice); i+= size {
+		end := i + size
+		if end > len(slice) {
+			end = len(slice)
 		}
-    maxjump = max(maxjump, i+int(steps[i]))
-		if maxjump == len(steps)-1 ||  len(steps) == 1 {
-			return true
-		}
+		chunks = append(chunks, slice[i:end])
 	}
-	return false
+	   fmt.Println(chunks)
 }
